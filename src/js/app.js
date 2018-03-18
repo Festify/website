67a652aaa7d5d4eeb6da7ddaf8f94c371095fefd
Leo Bernard <admin@leolabs.org>
2018-03-18 04:49:56 +0100

    :lipstick: Refine header style

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,12 @@ import {MDCRipple} from '@material/ripple';
 mdcAutoInit.register(MDCRipple.name, MDCRipple);
 
 mdcAutoInit();
+
+const header = document.querySelector('header');
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+        header.classList.add('scrolling');
+    } else {
+        header.classList.remove('scrolling');
+    }
+}, {passive: true});
